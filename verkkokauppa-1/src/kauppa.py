2c150a600956fc74675cefad35c7fc9b89df5157
Topi Pärssinen <parssinentopi@gmail.com>
2021-04-26 17:11:14 +0300

    muutetut nettikauppa

---
+++ verkkokauppa-1/src/kauppa.py
@@ -0,0 +1,34 @@
+from ostoskori import Ostoskori
+from pankki import Pankki as default_bank
+from viitegeneraattori import Viitegeneraattori as default_vg
+from varasto import Varasto as default_var
+
+class Kauppa:
+    def __init__(self, va=default_var, pa=default_bank, vi=default_vg ):
+        self._varasto = va
+        self._pankki = pa
+        self._viitegeneraattori = vi
+        self._kaupan_tili = "33333-44455"
+
+    def aloita_asiointi(self):
+        self._ostoskori = Ostoskori()
+
+    def poista_korista(self, id):
+        tuote = self._varasto.hae_tuote(id)
+        self._ostoskori.poista(tuote)
+        self._varasto.palauta_varastoon(tuote)
+
+    def lisaa_koriin(self, tuote_id):
+        print (tuote_id)
+        tsekki=self._varasto.saldo(self._varasto, tuote_id)
+        print (tsekki)
+        if tsekki > 0:
+            tuote = self._varasto.hae_tuote(self._varasto, tuote_id)
+            self._ostoskori.lisaa(tuote)
+            self._varasto.ota_varastosta(tuote)
+
+    def tilimaksu(self, nimi, tili_numero):
+        viite = self._viitegeneraattori.uusi()
+        summa = self._ostoskori.hinta()
+
+        return self._pankki.tilisiirto(nimi, viite, tili_numero, self._kaupan_tili, summa)
